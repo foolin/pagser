c02320cd989dbddd7a0a36b3f48da21cc7f7d16f
foolin <foolin@126.com>
2020-05-09 21:45:53 +0800

    add test errors.

---
+++ builtin_selections_test.go
@@ -0,0 +1,30 @@
+package pagser
+
+import (
+	"testing"
+)
+
+func TestBuiltinSelectionsErrors(t *testing.T) {
+	tests := []funcWantError{
+		//not args
+		{true, "eq", []string{}, ``},
+		//index not number
+		{true, "eq", []string{"a"}, ``},
+		//not args
+		{true, "parentsUntil", []string{}, ``},
+	}
+
+	for _, tt := range tests {
+		var sel = newTewSelection(tt.data)
+		_, err := builtinFuncs[tt.fun](sel, tt.args...)
+		if tt.want {
+			if err == nil {
+				t.Errorf("%v want an error", tt.String())
+			}
+			continue
+		}
+		if err != nil {
+			t.Errorf("%v want no error, but error is %v", tt.String(), err)
+		}
+	}
+}
